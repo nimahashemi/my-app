51701bdac16ea642ea239c8777145ec4fc06b79f
Nima Hashemi <n.hashemi@ebcom.ir>
2023-03-02 17:00:28 +0330

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  function showPopup(){
+    document.querySelector('.full-screen').classList.remove('hidden');
+  }
+  
+  function closePopup(){
+    document.querySelector('.full-screen').classList.add('hidden');
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Let me tell you a bit about popups</h3>
+      <button onClick={showPopup}>Show Popup</button>
+
+      <div className="full-screen hidden flex-container-center">
+        <h2 >Let me tell you about this thing!!</h2>
+        <button onClick={closePopup}>Close Popup!</button>
+      </div>
     </div>
   );
+
 }
 
 export default App;
